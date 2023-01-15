fea82d236d073e743fe591edf302119801fc696c
sakko.sama <saklism@gmail.com>
2023-01-15 20:34:59 +0700

    cb7c599

---
+++ _next/static/J9_6C-or8G_fyP88wFCbR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-3d25f3bbbcc10634.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],"/posts/[slug]":["static/chunks/pages/posts/[slug]-714726050bce2ac3.js"],sortedPages:["/","/_app","/_error","/posts/[slug]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
